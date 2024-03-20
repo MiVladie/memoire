97a1e644a5ad114326fca65d5e337cb246fa9c9f
MiVladie <justvladon@outlook.com>
2024-03-20 22:10:40 +0200

    🎎 fixed issues with SEO component

--- reve/src/config/constants.ts
+++ reve/src/config/constants.ts
@@ -1,3 +1,5 @@
 export const PROJECT_NAME = 'Mémoire';
+export const PROJECT_DESCRIPTION =
+	"'Mémoire is a project designed to keep track of listed & unlisted songs on music platforms of your choice'";
 
 export const PROD = process.env.NODE_ENV === 'production';
--- reve/src/hoc/Seo/Seo.tsx
+++ reve/src/hoc/Seo/Seo.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { PROJECT_NAME } from 'config/constants';
+import { PROJECT_DESCRIPTION, PROJECT_NAME } from 'config/constants';
 
 interface Props {
 	title?: string;
@@ -12,9 +12,7 @@ const Seo = ({ title, children }: Props) => (
 		<html lang='en' />
 
 		{typeof children === 'string' ? (
-			<title>
-				{children} | {PROJECT_NAME}
-			</title>
+			<title>{`${children} | ${PROJECT_NAME}`}</title>
 		) : (
 			<>
 				<title>{title ? `${title} | ${PROJECT_NAME}` : PROJECT_NAME}</title>
@@ -22,10 +20,7 @@ const Seo = ({ title, children }: Props) => (
 			</>
 		)}
 
-		<meta
-			name='description'
-			content='Mémoire is a project designed to keep track of listed & unlisted songs on music platforms of your choice'
-		/>
+		<meta name='description' content={PROJECT_DESCRIPTION} />
 	</>
 );
 
