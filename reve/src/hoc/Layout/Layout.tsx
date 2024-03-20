7215509501397663ef682e26d7b9decd6da76480
MiVladie <justvladon@outlook.com>
2024-03-20 22:11:06 +0200

    🗄 updated Layout component

--- reve/src/hoc/Layout/Layout.tsx
+++ reve/src/hoc/Layout/Layout.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
+
+import Circle from 'components/Circle/Circle';
 
 import * as classes from './Layout.module.scss';
 
@@ -7,10 +9,17 @@ import './reset.css';
 interface Props {
 	children: React.ReactNode;
 	className?: string;
+	style?: CSSProperties;
 }
 
-const Layout = ({ children, className }: Props) => (
-	<main className={[classes.Layout, className].join(' ')}>{children}</main>
-);
+const Layout = ({ children, className, style }: Props) => {
+	return (
+		<main className={[classes.Layout, className].join(' ')} style={style}>
+			{children}
+
+			<Circle />
+		</main>
+	);
+};
 
 export default Layout;
