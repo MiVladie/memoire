970ce24cc1356231179f6e85a8612e1d7c4f0a3c
MiVladie <justvladon@outlook.com>
2024-03-30 00:29:13 +0200

    🌴 displaying list of songs on home screen

--- reve/src/pages/home/index.tsx
+++ reve/src/pages/home/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 import { HeadFC } from 'gatsby';
-import { PLATFORMS_DATA } from 'config/data';
+import { PLATFORMS_DATA, PLAYLIST_DATA } from 'config/data';
 
 import Layout from 'hoc/Layout/Layout';
 import Platforms from 'containers/Platforms/Platforms';
+import Songs from 'containers/Songs/Songs';
 import Seo from 'hoc/Seo/Seo';
-
-import * as classes from './Home.module.scss';
 
 const Home = () => {
 	return (
-		<Layout className={classes.Layout}>
-			<Platforms data={PLATFORMS_DATA}>hello</Platforms>
+		<Layout>
+			<Platforms data={PLATFORMS_DATA}>
+				<Songs data={PLAYLIST_DATA.songs} />
+			</Platforms>
 		</Layout>
 	);
 };
