4b1136d915b87aa66ecad04bc920cdcbeace1ca3
MiVladie <justvladon@outlook.com>
2024-10-12 23:08:56 +0300

    💾 config/api update

--- reve/src/constants/api.ts
+++ reve/src/constants/api.ts
@@ -1,3 +1,3 @@
 import { PROD } from 'constants/config';
 
-export const API = PROD ? 'https://104.248.29.171/api' : 'http://localhost:8080/api';
+export const API = PROD ? 'https://letscomit.com/api' : 'http://localhost:8080/api';
