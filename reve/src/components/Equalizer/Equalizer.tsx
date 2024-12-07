6714eee4457af9d52cc9e84553d5cd8a5a8efdda
MiVladie <justvladon@outlook.com>
2024-12-08 01:07:10 +0200

    🎶 added Equalizer component

---
+++ reve/src/components/Equalizer/Equalizer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Equalizer.module.scss';
+
+const Equalizer = () => (
+	<div className={classes.Equalizer}>
+		<div />
+		<div />
+		<div />
+	</div>
+);
+
+export default Equalizer;
